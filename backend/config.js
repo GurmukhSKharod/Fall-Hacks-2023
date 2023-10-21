37afe927a7742cead294a1885b446ce3fb4ac334
GurmukhSKharod <gurmukhkharod@hotmail.com>
2023-10-21 14:15:09 -0700

    commit 3 - mongoose db setup working

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555; 
 
-
+export const mongoDBURL = 
+                "mongodb+srv://gurmukhkharod:UMMdcdDdftnROwTu@fall-hacks-2023-mern-ap.gf1jyup.mongodb.net/collection?retryWrites=true&w=majority"
