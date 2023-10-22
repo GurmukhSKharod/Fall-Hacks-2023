8af5778f6889a80480c617d7dbdaa6c85dc65eb3
iqkyM <djism.business@gmail.com>
2023-10-21 17:27:57 -0700

    commit : #2 macbook with/ user geolocation

--- frontend/src/MapContainer.jsx
+++ frontend/src/MapContainer.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const MapContainer = () => {
@@ -8,17 +7,34 @@ const MapContainer = () => {
     width: '100%',
   };
 
+  // Default center coordinates
   const defaultCenter = {
-    lat: 49.28165382092186,
-    lng: -122.91645297724942,
+    lat: 49.26418225596548, 
+    lng: -123.17626888135315
   };
 
+  // State to store user's geolocation
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const userLat = position.coords.latitude;
+        const userLng = position.coords.longitude;
+
+        setUserLocation({ lat: userLat, lng: userLng });
+      });
+    } else {
+      console.log('Geolocation is not available.'); // we can change this log to be an exception thrown instead of output to log. maybe???
+    }
+  }, []); // Run once when the component mounts
+
   return (
-    <LoadScript googleMapsApiKey={'AIzaSyBn31yotA0ODu_sVaT7nSoDu_yePcAJj7c'}>
+    <LoadScript googleMapsApiKey='AIzaSyBn31yotA0ODu_sVaT7nSoDu_yePcAJj7c'>
       <GoogleMap
         mapContainerStyle={mapStyles}
         zoom={15}
-        center={defaultCenter}
+        center={userLocation || defaultCenter} // Use userLocation if available, else use defaultCenter
       />
     </LoadScript>
   );
