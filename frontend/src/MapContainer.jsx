ac01b0fa029b525b34daa57fe1da0668e9c54c5e
GurmukhSKharod <gurmukhkharod@hotmail.com>
2023-10-21 18:15:53 -0700

    commit

--- frontend/src/MapContainer.jsx
+++ frontend/src/MapContainer.jsx
@@ -1,6 +1,7 @@
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
 
 const MapContainer = () => {
   const [universities, setUniversities] = useState([]);
